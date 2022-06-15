51a4e807e9ccabc508e6105761a7f9bec573fcaf
Thungghuan <1092887426@qq.com>
2022-06-15 21:05:42 +0800

    init project

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import Vue from '@vitejs/plugin-vue'
+import UnoCSS from 'unocss/vite'
+import { presetAttributify, presetUno } from 'unocss'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    Vue(),
+
+    UnoCSS({
+      presets: [presetUno(), presetAttributify()]
+    })
+  ]
+})
